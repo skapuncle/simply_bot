4942c8576e4cf9ffaaa289f3ade290a7cc66aa80
Yohn Carley <yohncarl@at-f4.msk.21-school.ru>
2022-08-27 20:15:50 +0300

    first stage

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main (String[] args) {
-        System.out.println("Hello! My name is Jarvis(v0.1");
+        System.out.println("Hello! My name is Jarvis(v0.1)");
         System.out.println("I was created in 2022");
     }
 }
