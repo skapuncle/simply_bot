c12b826eadfaa32d9ae7599d3b2e1d08f0b7f9dc
Danila Sokov <danila.sockov@yandex.ru>
2022-08-31 09:18:35 +0300

    stage 3

--- src/bot/Main.java
+++ src/bot/Main.java
@@ -10,5 +10,15 @@ public class Main {
 
         String name = scanner.nextLine();
         System.out.println("What a great name you have, " + name + "!");
+
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        int remainder3 = scanner.nextInt();
+        int remainder5 = scanner.nextInt();
+        int remainder7 = scanner.nextInt();
+        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+
+        System.out.println("Your age is " + age + " that's a good time to start programming!");
     }
 }
